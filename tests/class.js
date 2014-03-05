c28d55bf6e9cbf8dece3528bf81a41fddce6666a
James Long <longster@gmail.com>
2014-03-05 13:44:39 -0500

    destructure: don't use :expr on lhs

--- tests/class.js
+++ tests/class.js
@@ -1,89 +1,89 @@
 'use strict';
-var expect$679 = require('expect.js');
+var expect$702 = require('expect.js');
 describe('class', function () {
     it('should create a class', function () {
-        function FooEmpty$683() {
-            Object.getPrototypeOf(FooEmpty$683.prototype).constructor.call(this);
+        function FooEmpty$706() {
+            Object.getPrototypeOf(FooEmpty$706.prototype).constructor.call(this);
         }
-        function FooSimple$685(x$699, y$700) {
-            this.x = x$699;
-            this.y = y$700;
+        function FooSimple$708(x$719, y$720) {
+            this.x = x$719;
+            this.y = y$720;
         }
-        function FooWithMethod$689() {
-            Object.getPrototypeOf(FooWithMethod$689.prototype).constructor.call(this);
+        function FooWithMethod$712() {
+            Object.getPrototypeOf(FooWithMethod$712.prototype).constructor.call(this);
         }
-        FooWithMethod$689.prototype.length = function length$690() {
-            var x$704 = this.x;
-            var y$708 = this.y;
-            return x$704 * x$704 + y$708 * y$708;
+        FooWithMethod$712.prototype.length = function length() {
+            var x$724 = this.x;
+            var y$728 = this.y;
+            return x$724 * x$724 + y$728 * y$728;
         };
-        function Foo$692(x$709, y$710) {
-            this.x = x$709;
-            this.y = y$710;
+        function Foo$714(x$729, y$730) {
+            this.x = x$729;
+            this.y = y$730;
         }
-        Foo$692.prototype.length = function length$690() {
-            var x$714 = this.x;
-            var y$718 = this.y;
-            return x$714 * x$714 + y$718 * y$718;
+        Foo$714.prototype.length = function length() {
+            var x$734 = this.x;
+            var y$738 = this.y;
+            return x$734 * x$734 + y$738 * y$738;
         };
-        Foo$692.prototype.getX = function getX$693() {
+        Foo$714.prototype.getX = function getX() {
             return this.x;
         };
-        Foo$692.prototype.getY = function getY$694() {
+        Foo$714.prototype.getY = function getY() {
             return this.y;
         };
-        var f$698 = new Foo$692(1, 2);
-        expect$679(f$698.x).to.be(1);
+        var f$718 = new Foo$714(1, 2);
+        expect$702(f$718.x).to.be(1);
     });
     it('should support super', function () {
-        function Foo$720(x$733) {
-            this.fooX = x$733 + 5;
+        function Foo$740(x$747) {
+            this.fooX = x$747 + 5;
         }
-        Foo$720.prototype.getX = function getX$721() {
+        Foo$740.prototype.getX = function getX() {
             return this.fooX;
         };
-        function Bar$723(x$734) {
-            Object.getPrototypeOf(Bar$723.prototype).constructor.call(this, x$734);
-            this.barX = x$734;
+        function Bar$742(x$748) {
+            Object.getPrototypeOf(Bar$742.prototype).constructor.call(this, x$748);
+            this.barX = x$748;
         }
-        Bar$723.prototype = Object.create(Foo$720.prototype);
-        Bar$723.prototype.getX = function getX$721() {
+        Bar$742.prototype = Object.create(Foo$740.prototype);
+        Bar$742.prototype.getX = function getX() {
             return this.barX;
         };
-        Bar$723.prototype.getFooX = function getFooX$724() {
-            return Object.getPrototypeOf(Bar$723.prototype).getX.call(this);
+        Bar$742.prototype.getFooX = function getFooX() {
+            return Object.getPrototypeOf(Bar$742.prototype).getX.call(this);
         };
-        Bar$723.prototype.nested = function nested$725() {
+        Bar$742.prototype.nested = function nested() {
             if (true) {
                 if (this.barX > 2) {
-                    return Object.getPrototypeOf(Bar$723.prototype).getX.call(this);
+                    return Object.getPrototypeOf(Bar$742.prototype).getX.call(this);
                 }
             }
             return 1;
         };
-        Bar$723.prototype.nestedFunction = function nestedFunction$726() {
-            var self$740 = this;
-            function run$741() {
+        Bar$742.prototype.nestedFunction = function nestedFunction() {
+            var self$754 = this;
+            function run$755() {
                 if (true) {
-                    return Object.getPrototypeOf(Bar$723.prototype).getX.call(self$740);
+                    return Object.getPrototypeOf(Bar$742.prototype).getX.call(self$754);
                 }
             }
-            return run$741();
+            return run$755();
         };
-        Bar$723.prototype.getMethod = function getMethod$727() {
-            return Object.getPrototypeOf(Bar$723.prototype).getX;
+        Bar$742.prototype.getMethod = function getMethod() {
+            return Object.getPrototypeOf(Bar$742.prototype).getX;
         };
-        Bar$723.prototype.getMethod2 = function getMethod2$728() {
-            return Object.getPrototypeOf(Bar$723.prototype)['getX'];
+        Bar$742.prototype.getMethod2 = function getMethod2() {
+            return Object.getPrototypeOf(Bar$742.prototype)['getX'];
         };
-        var b$732 = new Bar$723(5);
-        expect$679(b$732.fooX).to.be(10);
-        expect$679(b$732.barX).to.be(5);
-        expect$679(b$732.getX()).to.be(5);
-        expect$679(b$732.getFooX()).to.be(10);
-        expect$679(b$732.nested()).to.be(10);
-        expect$679(b$732.nestedFunction()).to.be(10);
-        expect$679(expect$679(b$732.getMethod().call(b$732)).to.be(10));
-        expect$679(expect$679(b$732.getMethod2().call(b$732)).to.be(10));
+        var b$746 = new Bar$742(5);
+        expect$702(b$746.fooX).to.be(10);
+        expect$702(b$746.barX).to.be(5);
+        expect$702(b$746.getX()).to.be(5);
+        expect$702(b$746.getFooX()).to.be(10);
+        expect$702(b$746.nested()).to.be(10);
+        expect$702(b$746.nestedFunction()).to.be(10);
+        expect$702(expect$702(b$746.getMethod().call(b$746)).to.be(10));
+        expect$702(expect$702(b$746.getMethod2().call(b$746)).to.be(10));
     });
 });
